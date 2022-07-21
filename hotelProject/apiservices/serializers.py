27d97c4f09bddd1a7d629a4bd2c3fead84fff473
unknown <nacwork85@gmail.com>
2022-07-21 09:04:27 +0300

    update 4 services api

--- hotelProject/apiservices/serializers.py
+++ hotelProject/apiservices/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 # now import models from models.py
 from roomManager.models import Service
+from currencyapp.models import Currency
 
 
 # create a models serializer
 
-class ServicesSerializer(serializers.HyperlinkedModelSerializer):
+
+class currencySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Currency
+        fields = ('name', 'symbole')
+
+
+class ServicesSerializer(serializers.ModelSerializer):
+    currency = currencySerializer(many=True)
+
     class Meta:
         model = Service
-        fields = ('name', 'name_ar', 'price', 'description')
+        fields = ('name', 'name_ar', 'price', 'currency', 'description')
